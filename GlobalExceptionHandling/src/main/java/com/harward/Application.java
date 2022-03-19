456746ff9e61a2ccb5b8a934340a59243352af28
parvesh <parvesh.sri@gmail.com>
2022-03-19 22:20:24 +0530

    noerror

--- GlobalExceptionHandling/src/main/java/com/harward/Application.java
+++ GlobalExceptionHandling/src/main/java/com/harward/Application.java
@@ -9,7 +9,6 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         SpringApplication.run(Application.class, args);
-        //hello this is git and my name is parvesh
-        //my name is rmauS  hello thu sskqlkxkxaxa
+      
     }
 }
