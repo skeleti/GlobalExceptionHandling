91dcf787e9491f64f5973ff57159fa0921d0bcd2
parvesh <parvesh.sri@gmail.com>
2022-03-19 15:02:07 +0530

    updated application file

--- GlobalExceptionHandling/src/main/java/com/harward/Application.java
+++ GlobalExceptionHandling/src/main/java/com/harward/Application.java
@@ -9,6 +9,6 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         SpringApplication.run(Application.class, args);
-        SpringApplication.run(Application.class, args);
+        //hello this is git
     }
 }
